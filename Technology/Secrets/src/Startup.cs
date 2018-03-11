8d8a29c66e02a530ca6a1b922fea2eeb4ffd50be
Frank Pommerening <frank@pommerening-online.de>
2018-03-11 21:30:28 +0100

    Add technology samples

---
+++ Technology/Secrets/src/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Nancy.Owin;
+
+namespace FP.Spartakiade2018.Secrets
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseOwin(x => x.UseNancy());
+        }
+    }
+}
