30c1e57a0739e77657277b5caefb749efffc62ff
Frank Pommerening <frank@pommerening-online.de>
2018-03-11 22:10:44 +0100

    Add basics samples

---
+++ Basics/Hallo-Custom/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FP.Spartakiade2018.Hallo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hallo OpenFAAS {DateTime.Now}");
+        }
+    }
+}
